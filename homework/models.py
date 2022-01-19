2b5d6c617e79d54064562eaba0e55271668571cb
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-01-19 23:03:46 +0900

    update: model 수정

--- homework/models.py
+++ homework/models.py
@@ -5,5 +5,12 @@ from django.contrib.auth.models import User
 class Homework(models.Model):
   title = models.CharField(max_length=200)
   writer = models.ForeignKey(User, on_delete=models.CASCADE)
+  contents = models.TextField()
+
+class Assignment(models.Model):
+  hw_id = models.ForeignKey(Homework, on_delete=models.CASCADE, db_column="hw_id")
+  title = models.CharField(max_length=200)
+  writer = models.ForeignKey(User, on_delete=models.CASCADE)
   url = models.URLField(max_length=200)
   contents = models.TextField()
+  pub_date = models.DateTimeField()
